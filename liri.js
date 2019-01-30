b8e1a53b5f6348e1ab695241e97d8593f70efa8f
pmutunga <paskwa.mutunga@ipsafari.com>
2019-01-30 00:21:31 -0500

    added do-what-it-says logic

--- liri.js
+++ liri.js
@@ -22,12 +22,15 @@ var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var defaultSong = "Ace of Base The Sign";
 var defaultConcert = "Pink";
+var searchText = " ";
+var searchThis = " ";
+var userChoice = " ";
 
 // TO-DO: capture inputCommand and inputSearch - > consert-this; spotify-this-song; movie-this, do-what it says
 
 //capture user input
 
-var userChoice = process.argv[2];
+userChoice = process.argv[2];
 var userSearch = process.argv[3]; 
 
 
@@ -55,58 +58,56 @@ console.log(userGenericSearch);
 
 function processInput(doThis, userSearchArray){
 
-  //how do I pass the value of doThis and searchThis to processInput? - Pass the variables as arguments
+    //how do I pass the value of doThis and searchThis to processInput? - Pass the variables as arguments
     console.log("User would like to " + doThis + " for " + userSearchArray);
-	//console.log(inputCommand);
+    //console.log(inputCommand);
+  
+   
+      switch(doThis){
 
-	switch(doThis){
+      case "concert-this":
+              if(userSearchArray.length ===0){
+              searchThis = undefined;
+              concertThis(searchThis);}
+              else {
+              searchThis = userSearchArray.join(" ");
+              console.log(doThis + " " + searchThis); //test user inputs
+              console.log("concerts coming up"); 
+              concertThis(searchThis)}; break;
+      case "spotify-this-song":
+            //If user has not specified a song , use default
+            if(userSearchArray.length ===0){
+              searchThis = undefined;
+              spotifyThis(searchThis);}
+              else {
+            // console.log(defaultSong);
+              searchThis = userSearchArray.join(" ");
+              console.log(doThis + " " + searchThis); //test user inputs
+              console.log("your search for " + searchThis + " coming up");
+              console.log("run spotifythis function +======================+")
+              //saveSearch(userChoice, searchThis);
+              spotifyThis(searchThis)}; break;
+      
+      case "movie-this":
+            if(userSearchArray.length ===0){
+              searchThis = undefined;
+              movieThis(searchThis);}
+              else {
+            searchThis = userSearchArray.join("+");
+            var saveSearchThis= userSearchArray.join(" ");
+            console.log("Save this search " + saveSearchThis);
+            // console.log(doThis + " " + searchThis); //test user inputs
+            movieThis(searchThis, saveSearchThis)}; break;
+      case "do-what-it-says":
+          console.log(doThis); //test user inputs
+          doWhatItSays(); break;
+      default: 
+      console.log("Invalid command. Please type any of the following commnds: concert-this spotify-this-song movie-this or do-what-it-says");
+        } //end of case
+    
+} //end of processInput 
 
-    case "concert-this":
-          if(userSearchArray.length ===0){
-            searchThis = undefined;
-            concertThis(searchThis);} 
-            else {
-            searchThis = userSearchArray.join(" ");
-            console.log(doThis + " " + searchThis); //test user inputs
-            console.log("concerts coming up"); 
-            concertThis(searchThis)
-            }; break;
-	  case "spotify-this-song":
-          //If user has not specified a song , use default
-          // console.log(defaultSong);
-          if(userSearchArray.length ===0){
-            searchThis = undefined;
-            console.log("run spotify-this function for " + searchThis + "+======================+");
-            spotifyThis(searchThis);
-          } else {
-            searchThis = userSearchArray.join(" ");
-            console.log(doThis + " " + searchThis); //test user inputs
-            console.log("your search for " + searchThis + " coming up");
-            console.log("run spotifythis function +======================+")
-            
-            spotifyThis(searchThis)}; break;
-     
-	  case "movie-this":
-          //If user has not specified a movie Name , use default
-          if(userSearchArray.length === 0){
-            searchThis = undefined;
-            console.log("movie info coming up for " + searchThis); //test user inputs
-            movieThis(searchThis); 
-          }    else {
-          searchThis = userSearchArray.join("+");
-          // console.log(doThis + " " + searchThis); //test user inputs
-          console.log("movie info coming up for " + searchThis); //test user inputs
-          movieThis(searchThis); 
-          } break;
-	/*case "do-what-it-says":
-        // doWhatItSays(); break;
-        console.log(doThis + " " + searchThis); //test user inputs
-        console.log("whatever coming up"); break;*/
-	default: 
-		console.log("Invalid command. Please type any of the following commnds: concert-this spotify-this-song movie-this or do-what-it-says");
-}
 
-}
 // concert-this++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //node liri.js concert-this <artist/band name here>
 // search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
@@ -124,22 +125,31 @@ function concertThis(searchThis){
     //Get concert data using request. TO-DO - understand how request works.
 
     request(concertURL, function (error, response, body) {
+
+      if(error){
         console.log('error:', error); // Print the error if one occurred
+      } else {
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         var json= JSON.parse(response.body);
         //console.log(json[0]);
         var thisEvent = json[0];
         //console.log("+++++++++++++End of Bands in Town data ++++++++++++++++++++++++++++");
-        var artist = thisEvent.lineup;
-        console.log("Artist: " + artist);
+        // var artist = thisEvent.lineup;
+        // console.log("Artist: " + artist);
         var concertVenue = thisEvent.venue.name;
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         console.log("Concert Venue: " + concertVenue);
         var concertLocation = thisEvent.venue.city;
         console.log("Event location: " + concertLocation);
         var concertDate = thisEvent.datetime;
         console.log(moment(concertDate).format("MM/DD/YYYY"));
+        console.log("========================================================");
     
-        //console.log(" The name of the venue is: " + concertVenue + "\nThe Venue location is: " + concertLocation + "\n The date of the Event is: " + moment.format(concertDate); //(use moment to format this as "MM/DD/YYYY")
+       saveSearchData = "\n" + searchThis + "\n_____________________________"+ "\nConcert Venue: " + concertVenue + "\nEvent location: " + concertLocation + "\n" + moment(concertDate).format("MM/DD/YYYY") + "\n==================================" + "\n";
+
+       saveSearch(saveSearchData);
+ 
+      } //end of else
       });
 
 }
@@ -159,74 +169,28 @@ function spotifyThis(searchThis){
 
     //fetch data from spotify api
       spotify.search({ type: 'track', query: searchThis }).then(function(response) {
-      console.log(response);
+      console.log(response); 
+      
+      //saveSearch(userChoice, searchThis, searchData);//save uer search in random.txt using fs
       console.log("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       //console.log("Artist:" + response.artist "\nThe song's name" + response.songName + "\nA preview link of the song from Spotify" + response.Preview + "\nThe album that the song is from" + response.Album);
-   console.log("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
+      console.log("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
       console.log("Artists: " + response.tracks.items[0].artists[0].name);
       console.log("Song Name: " + response.tracks.items[0].name);
       console.log("Preview URL: " + response.tracks.items[0].preview_url);
-      console.log("Album: " + response.tracks.items[0].album.name);
+      console.log("Preview URL: " + response.tracks.items[0].preview_url);
       console.log("|______________________________________|")
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-      
 
-  
-}
+      saveSearchData = "\n" + searchThis + "\n_____________________________" + "\nArtists: " + response.tracks.items[0].artists[0].name + "\nSong Name: " + response.tracks.items[0].name + "\nPreview URL: " + response.tracks.items[0].preview_url + "\n==================================" + "\n";
 
+      saveSearch(saveSearchData);
 
+  }); //end of spotify search
+} //end of spotify-this
 // movie-this========================================================
-/*node liri.js movie-this '<movie name here>'
 
-This will output the following information to your terminal/bash window:
 
-  * Title of the movie.
-  * Year the movie came out.
-  * IMDB Rating of the movie.
-  * Rotten Tomatoes Rating of the movie.
-  * Country where the movie was produced.
-  * Language of the movie.
-  * Plot of the movie.
-  * Actors in the movie.
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-
-OMDB API requires an API key. You may use trilogy.
-It's on Netflix!*/
-
-//Test Movie using axios
-/*function movie(movieURL){
-    console.log(movieURL);
-
-  axios.get(movieURL).then(
-  function(response) {
-    console.log("The title of the movie is:" + response.data.Title) // * Title of the movie.
-    console.log("The year the movie was released is:" + response.data.Year)// * Year the movie came out.
-    console.log("The IMDB Rating of the movie is:" + response.data.imdbRating)// * IMDB Rating of the movie.
-
-    //get Rotten tomatoes rating
-    var ratings = response.data.Ratings;
-    var rottenT;
-    ratings.forEach(function(source) {
-      
-      if(source.Source === "Rotten Tomatoes"){
-        rottenT = source.Value;
-      }
-    });
-    console.log("The Rotten Tomatoes Rating of the movie of the movie is:" + rottenT);// * Rotten Tomatoes Rating of the movie.
-    console.log("The country where the moview was produced is: " + response.data.Country)// * Country where the movie was produced.
-    console.log("The language of the movie is: " + response.data.Language);// * Language of the movie.
-    console.log("The plot of the move is: " + response.data.Plot);// * Plot of the movie.
-    console.log("The actors in the movie are: " + response.data.Actors);// * Actors in the movie.
-  }
-);
-} */
-
-function movieThis(searchThis){
+function movieThis(searchThis, searchToSave){
 
  //   If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
@@ -266,12 +230,61 @@ function movieThis(searchThis){
         console.log("Plot: " + json.Plot);
         console.log("Actors: " + json.Actors);
         console.log("==================================");
+
+        saveSearchData = "\n" + searchToSave + "\n_____________________________" + "\nTitle: " + json.Title + "\nYear Released: " + json.Year + "\nIMDB Rating: " + json.imdbRating + "\nRotten Tomatoes Rating:" + rtRating + "\nYear Released: " + json.Year + "\nIMDB Rating: " + json.imdbRating + "\nRotten Tomatoes Rating:" + rtRating + "\nCountry: " + json.Country + "\nLanguage: " + json.Language + "\nPlot: " + json.Plot + "\nActors: " + json.Actors + "\n==================================" + "\n";
+        //save output to log.txt
+        saveSearch(saveSearchData);
+        // fs.appendFile("log.text", saveSearchData, function(err){
+        //   if(err){
+        //     console.log(err)
+        //   } else{
+        //     console.log("content added!")
+        //   }
+        // }) //end of fs
     
       });
     }
   }
 
-//test
+function doWhatItSays(){
+  fs.readFile("random.txt", "utf8", function (err, data) {
+    if (err) {
+      return console.log(err)
+
+    } else {
+      console.log(data);
+      var doWhatArr = data.split(";");
+      console.log(doWhatArr);
+
+       // Loop Through the newly created output array
+  for (var i = 0; i < doWhatArr.length; i++) {
+
+    // Print each element (item) of the array/
+    console.log(doWhatArr[i]);
+    var searchdata = doWhatArr[i].split(",");
+    console.log(searchdata[0]);
+    console.log(searchdata[1]);
+    userChoice = searchdata[0];
+    searchThis = searchdata[1];
+    console.log("userchoice: " + userChoice + " userSearchArray: " + userSearchArray);
+    switch(userChoice) {
+      case "movie-this":
+      movieThis(searchThis); break;
+      case "concert-this":
+      concertThis(searchThis); break;
+      case "spotify-this-song":
+      spotifyThis(searchThis); break;
+      default:
+      console.log("Enjoy movies, concerts and songs with liri");
+    }
+  }
+
+    }
+  }); //end of fs
+
+} //end of dowhatitsays()
+
+  //test
 
 //test user input, moment
 function testApp(){
@@ -289,6 +302,26 @@ function testApp(){
 });
 }
 
+
+//Add search to random.txt
+
+  function saveSearch(searchData) {
+    // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    // console.log("Save this search " + saveCommand + " , " + saveSearch);
+    // searchText = "\n \" " + saveCommand + "," + "\" \" " + saveSearch + "\n";
+    fs.appendFile("log.txt", searchData, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    
+      //random.txt
+      // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+      else {
+        console.log("Search Content Added!");
+      }
+    });
+  }
+  
 
 //call functions+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
